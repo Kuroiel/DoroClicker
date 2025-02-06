766d7d5ea241aea8a4a95d3620ed9624ca41deea
unknown <felixjlee69@gmail.com>
2025-02-06 15:14:52 -0800

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 // jest.config.js
-export default {
+module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest'
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!lodash-es/)'  // Only needed if using ES modules in node_modules
-  ]
+  }
 };
