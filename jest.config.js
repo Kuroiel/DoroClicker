3e273dbb7311be49171cc58ec8c7ee4d966fd716
unknown <felixjlee69@gmail.com>
2025-02-08 13:24:43 -0800

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-export default {
+module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/tests/unit/**/*.test.js'],
-  transform: {}
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest'
+  }
 };
