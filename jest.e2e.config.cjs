efae5e9a6120a2a2afb86789e23ecc4e468d6b15
unknown <felixjlee69@gmail.com>
2025-02-08 12:49:54 -0800

    tests

--- jest.e2e.config.cjs
+++ jest.e2e.config.cjs
@@ -1,4 +1,3 @@
-// jest.e2e.config.cjs
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/e2e-tests/**/*.test.cjs']
--- jest.unit.config.mjs
+++ jest.unit.config.mjs
@@ -1,4 +1,3 @@
-// jest.config.mjs
 export default {
   testEnvironment: 'jsdom',
   testMatch: ['**/tests/unit/**/*.test.js'],
