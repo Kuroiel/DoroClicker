1345638bdba77a17e00fbf015de23a2a10b23b0b
unknown <felixjlee69@gmail.com>
2025-02-14 19:34:13 -0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    adapter({
+      fallback: 'index.html'
+    })
+  ],
+  base: './', // Critical for GitHub Pages
   build: {
     target: 'esnext',
     assetsInlineLimit: 0
-  },
-  server: {
-    port: 3000
   }
 });
